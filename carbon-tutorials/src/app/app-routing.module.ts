f34c47a01a6f655ff3c4376c505c725e7da542ca
Pablo <luis.pablo.lopez.lopez@uabc.edu.mx>
2021-02-18 23:00:31 -0700

    chore(structure): add project structure with basic routing

---
+++ carbon-tutorials/src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+// Angular Core Elements
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+// App components/elements
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: '**', redirectTo: '' }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
